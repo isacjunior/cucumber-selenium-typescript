a88c631ab4c652503ea777004d36d2afc521f48f
Isac <isac_asj91@hotmail.com>
2019-04-04 14:15:10 -0300

    add report

---
+++ report/index.js
@@ -0,0 +1,15 @@
+import reporter from 'cucumber-html-reporter'
+
+const options = {
+  theme: 'bootstrap',
+  jsonFile: 'report/cucumber_report.json',
+  output: 'report/cucumber_report.html',
+  screenshotsDirectory: 'screenshots/',
+  reportSuiteAsScenarios: true,
+  launchReport: true,
+  metadata: {
+    'App Version': '1.0.0'
+  }
+}
+
+reporter.generate(options)
